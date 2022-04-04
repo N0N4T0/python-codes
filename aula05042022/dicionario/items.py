7287edebe5e298906d84076d951ea08eeb735356
LUCIO Notebook <luciononato@hotmail.com>
2022-04-04 02:30:59 -0300

    diocionários

---
+++ aula05042022/dicionario/items.py
@@ -0,0 +1,13 @@
+# Demosntração items
+dados_cidade = {
+    'nome': 'São Paulo',
+    'estado': 'São Paulo',
+    'restaurantes' : {
+        'boteco do Franca' : "Rua villa veede",
+        'boteco do Americo' : "Rua Villa Amarela"
+    },
+    'area_km2': 1521,
+    'populacao_milhoes': 12.18,
+}
+
+print(dados_cidade.items()) 
